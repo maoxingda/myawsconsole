44d2d7c7920dd64e1d0807556b6ac04f68bbee51
maoxd <maoxingda@meican.com>
2023-06-14 08:03:14 +0800

    修改app名称为中文

--- dms/apps.py
+++ dms/apps.py
@@ -4,3 +4,4 @@ from django.apps import AppConfig
 class DmsConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'dms'
+    verbose_name = '数据迁移服务'
--- redshift/apps.py
+++ redshift/apps.py
@@ -4,3 +4,4 @@ from django.apps import AppConfig
 class RedshiftConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'redshift'
+    verbose_name = '离线数仓'
