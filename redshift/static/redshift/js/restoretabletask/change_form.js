7b03da0abf6ba90f55fb7743a3ed49e79c02542c
maoxd <maoxingda@meican.com>
2023-06-16 22:06:13 +0800

    状态更新

--- redshift/static/redshift/js/restoretabletask/change_form.js
+++ redshift/static/redshift/js/restoretabletask/change_form.js
@@ -2,11 +2,14 @@
 {
     django.jQuery(function () {
         const btn_running = django.jQuery('#id_running');
+        const status = django.jQuery('#restoreclustertask_form > div > fieldset > div.form-row.field-status > div > div');
         django.jQuery('#id_launch').click(function () {
             django.jQuery(this).toggle();
             btn_running.toggle();
+                status.text('RUNNING');
             django.jQuery.get(django.jQuery(this).prop('href'), function () {
                 btn_running.text('已完成...');
+                status.text('COMPLETED');
             });
         });
     });
