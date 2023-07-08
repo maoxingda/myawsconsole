1fe97aa7b3d5b6208c6cc6394e19e56d3a263c60
maoxd <maoxingda@meican.com>
2023-07-08 23:46:00 +0800

    删除35天之前的快照

--- redshift/apps.py
+++ redshift/apps.py
@@ -2,6 +2,12 @@ from django.apps import AppConfig
 
 
 class RedshiftConfig(AppConfig):
+    def ready(self):
+        super().ready()
+        from redshift.models import Snapshot
+        from datetime import datetime, timedelta
+        Snapshot.objects.filter(create_time__lte=datetime.utcnow() - timedelta(days=35)).delete()  # redshift集群快照最多能保留35天
+
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'redshift'
     verbose_name = '离线数仓'
