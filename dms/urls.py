1de42494b11d9eef771b287f7b3b6f33108f19a2
maoxd <maoxingda@meican.com>
2023-12-24 11:54:14 +0800

    代码格式化

--- dms/urls.py
+++ dms/urls.py
@@ -5,8 +5,8 @@ from dms import views
 app_name = 'dms'
 
 urlpatterns = [
-    path('task/refresh/', views.refresh_tasks, name='refresh_tasks'),
-    path('task/<int:task_id>/download_table_mapping/', views.download_table_mapping, name='download_table_mapping'),
-    path('table/refresh/<int:task_id>/', views.refresh_tables, name='refresh_tables'),
-    path('endpoint/refresh/', views.refresh_endpoints, name='refresh_endpoints'),
+    path('task/refresh/'                             , views.refresh_tasks         , name = 'refresh_tasks'),
+    path('task/<int:task_id>/download_table_mapping/', views.download_table_mapping, name = 'download_table_mapping'),
+    path('table/refresh/<int:task_id>/'              , views.refresh_tables        , name = 'refresh_tables'),
+    path('endpoint/refresh/'                         , views.refresh_endpoints     , name = 'refresh_endpoints'),
 ]
--- redshift/urls.py
+++ redshift/urls.py
@@ -5,11 +5,11 @@ from redshift import views
 app_name = 'redshift'
 
 urlpatterns = [
-    path('cluster/refresh/', views.refresh_clusters, name='refresh_clusters'),
-    path('snapshot/refresh/', views.refresh_snapshots, name='refresh_snapshots'),
-    path('table/refresh/', views.refresh_tables, name='refresh_tables'),
-    path('launch_restore_table_task/<int:task_id>/', views.launch_restore_table_task, name='launch_restore_table_task'),
-    path('launch_restore_cluster_task/<int:task_id>/', views.launch_restore_cluster_task, name='launch_restore_cluster_task'),
-    path('download_sql/<int:task_id>/', views.download_sql, name='download_sql'),
-    path('batch_download_sql/', views.batch_download_sql, name='batch_download_sql'),
+    path('cluster/refresh/'                          , views.refresh_clusters           , name = 'refresh_clusters'),
+    path('snapshot/refresh/'                         , views.refresh_snapshots          , name = 'refresh_snapshots'),
+    path('table/refresh/'                            , views.refresh_tables             , name = 'refresh_tables'),
+    path('launch_restore_table_task/<int:task_id>/'  , views.launch_restore_table_task  , name = 'launch_restore_table_task'),
+    path('launch_restore_cluster_task/<int:task_id>/', views.launch_restore_cluster_task, name = 'launch_restore_cluster_task'),
+    path('download_sql/<int:task_id>/'               , views.download_sql               , name = 'download_sql'),
+    path('batch_download_sql/'                       , views.batch_download_sql         , name = 'batch_download_sql'),
 ]
