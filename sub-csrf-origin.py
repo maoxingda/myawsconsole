b9c04691e5e25e34db80b12a72403d7aed4cce4a
maoxd <maoxingda@meican.com>
2025-02-17 18:41:15 +0800

    add sub-csrf-origin.py

---
+++ sub-csrf-origin.py
@@ -0,0 +1,15 @@
+import re
+import sys
+
+if __name__ == '__main__':
+    origin = sys.argv[1]
+
+    with open('myawsconsole/settings.py') as f:
+        settings = f.read()
+
+    csrf_origin_pattern = re.compile(r"'https://\w+\.\w+\.cpolar\.top'")
+
+    settings = csrf_origin_pattern.sub(f"'{origin}'", settings)
+
+    with open('myawsconsole/settings.py', 'w') as f:
+        f.write(settings)
