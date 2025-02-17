61f41a71812d3c13a1692805951219759f153457
maoxd <maoxingda@meican.com>
2025-02-17 18:30:03 +0800

    CSRF_TRUSTED_ORIGINS

--- myawsconsole/settings.py
+++ myawsconsole/settings.py
@@ -132,6 +132,10 @@ STATICFILES_DIRS = [
     BASE_DIR / "static",
 ]
 
+CSRF_TRUSTED_ORIGINS = [
+    'https://75170238.r17.cpolar.top',
+]
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
